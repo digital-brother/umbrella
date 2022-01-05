8c2733c6bdf6e48681790ae0ddc65440bcbebd72
Janmenjoy Roy <janmenjoy@alumnux.com>
2022-01-05 15:36:29 +0530

    changed the default postgres password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "umbrella",
         "USER": "postgres",
-        "PASSWORD": "fruit1",
+        "PASSWORD": "postgres",
         "HOST": env("DB_HOST"),
         "PORT": "5432",
     }
