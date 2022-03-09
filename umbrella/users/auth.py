f7e467b4b628155b10ee558349ce268d792b930d
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-03-09 22:44:01 +0200

    Cache JWT token

--- umbrella/users/auth.py
+++ umbrella/users/auth.py
@@ -1,34 +1,48 @@
 import jwt
+import time
+
 from django.conf import settings
-from jwt import PyJWTError
+from django.contrib.auth import get_user_model
 from mozilla_django_oidc.contrib.drf import OIDCAuthentication
 from rest_framework import exceptions
 
+User = get_user_model()
+
 
 class DynamicRealmOIDCAuthentication(OIDCAuthentication):
-    def get_realm_url_from_jwt_token(self, request):
+    def get_access_token_payload(self, request):
         encoded_access_token = self.get_access_token(request)
         if not encoded_access_token:
             return None
 
         try:
-            access_token = jwt.decode(encoded_access_token, options={"verify_signature": False})
-        except PyJWTError as exc:
+            access_token_payload = jwt.decode(encoded_access_token, options={"verify_signature": False})
+        except jwt.PyJWTError:
             msg = f"Invalid JWT token"
             raise exceptions.AuthenticationFailed(msg)
 
-        keycloak_realm_url = access_token.get('iss')
+        return access_token_payload
 
-        if not keycloak_realm_url:
-            msg = 'JWT token iss field is not present or empty'
-            raise exceptions.AuthenticationFailed(msg)
-
-        return keycloak_realm_url
+    def get_cached_jwt_token_user(self, request):
+        expiration = request.session.get('oidc_id_token_expiration', 0)
+        now = time.time()
+        if expiration > now:
+            access_token_payload = self.get_access_token_payload(request)
+            user = User.objects.get(email=access_token_payload['email'])
+            return user
 
     def authenticate(self, request):
-        keycloak_realm_url = self.get_realm_url_from_jwt_token(request)
+        user = self.get_cached_jwt_token_user(request)
+        if user:
+            return user, self.get_access_token(request)
+
+        access_token_payload = self.get_access_token_payload(request)
+        keycloak_realm_url = access_token_payload['iss']
         self.backend.OIDC_OP_TOKEN_ENDPOINT = settings.OIDC_OP_TOKEN_ENDPOINT_TEMPLATE.format(
             keycloak_realm_url=keycloak_realm_url)
         self.backend.OIDC_OP_USER_ENDPOINT = settings.OIDC_OP_USER_ENDPOINT_TEMPLATE.format(
             keycloak_realm_url=keycloak_realm_url)
-        return super().authenticate(request)
+        user, access_token = super().authenticate(request)
+
+        request.session['oidc_id_token_expiration'] = access_token_payload['exp']
+        return user, access_token
