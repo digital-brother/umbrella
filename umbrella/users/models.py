cd43e0fe4329c3da3a294978f74c792958d178ff
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-03-15 22:58:21 +0200

    Refactor User.realm

--- umbrella/users/models.py
+++ umbrella/users/models.py
@@ -1,10 +1,10 @@
 import uuid
-from django.db import models
+
 from django.conf import settings
-from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models.signals import post_save
-from django.utils.translation import gettext_lazy as _
+from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
@@ -12,7 +12,7 @@ class User(AbstractUser):
     NO_REALM = 'no_realm'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    realm = models.CharField(_("User realm"), default=NO_REALM, max_length=255)
+    realm = models.CharField(default=NO_REALM, max_length=255)
 
     def __str__(self):
         return self.username
