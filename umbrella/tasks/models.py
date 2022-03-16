2888db56b300b2cb04501ef9c81cabf93794e028
Alex-Shvab <apamic1995@gmail.com>
2022-03-16 15:39:17 +0200

    first setup tasks

---
+++ umbrella/tasks/models.py
@@ -0,0 +1,63 @@
+from datetime import date, timedelta
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from umbrella.contracts.models import Lease
+from umbrella.tasks.choices import ProgressChoices, RepeatsChoices, PeriodChoices, \
+    WhenChoices
+
+User = get_user_model()
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
+def two_days_ahead():
+    return date.today() + timedelta(days=2)
+
+
+class Task(models.Model):
+    # Contract info
+    contract = models.ForeignKey(Lease, on_delete=models.CASCADE)
+    clause_type = models.CharField(max_length=128)
+    bl_type = models.CharField(max_length=128)
+    link_to_text = models.CharField()
+
+    title = models.CharField(max_length=128)
+    assigned_to = models.ManyToManyField(User, related_name="executors")
+    due_date = models.DateField()
+    progress = models.CharField(
+        max_length=32, choices=ProgressChoices.choices, default=ProgressChoices.NOT_STARTED
+    )
+    notes = models.TextField(null=True, blank=True)
+    # status = models.CharField(
+    #     max_length=32, choices=RepeatsChoices.choices, default=RepeatsChoices.EVERYDAY
+    # )
+
+    # Reminder
+    number = models.PositiveIntegerField()
+    period = models.CharField(
+        max_length=32, choices=PeriodChoices.choices, default=PeriodChoices.DAYS
+    )
+    when = models.CharField(
+        max_length=32, choices=WhenChoices.choices, default=WhenChoices.BEFORE
+    )
+    repeats = models.CharField(
+        max_length=32, choices=RepeatsChoices.choices, default=RepeatsChoices.EVERYDAY
+    )
+    until = models.DateField(default=two_days_ahead())
+
+
+class ChecklistModel(models.Model):
+    title = models.CharField(max_length=128)
+    is_done = models.BooleanField(default=False)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+
+class TaskCommentModel(models.Model):
+    message = models.CharField(max_length=1024)
+    created_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
