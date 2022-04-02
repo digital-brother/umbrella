be3de8f36cff69b793f962dd168236bd2d78854c
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-02 15:12:08 +0300

    Adds tests for lease create endpoint

--- umbrella/contracts/urls.py
+++ umbrella/contracts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from umbrella.contracts.views import GetAddFilePresignedUrlView, LeaseListView
 
 urlpatterns = [
-    path('get-add-file-presigned-url/', GetAddFilePresignedUrlView.as_view(), name='get_add_file_presigned_url'),
+    path('get-add-file-presigned-url/', GetAddFilePresignedUrlView.as_view(), name='lease-create'),
     path('uploads/', LeaseListView.as_view(), name='lease-list'),
 ]
--- umbrella/tests/test_contracts.py
+++ umbrella/tests/test_contracts.py
@@ -1,3 +1,6 @@
+from unittest import mock
+from unittest.mock import Mock
+
 import factory
 import pytest
 from django.urls import reverse
@@ -9,13 +12,26 @@ from umbrella.users.test.factories import UserFactory
 pytestmark = pytest.mark.django_db
 
 
-def test_lease_list(client, lease):
+def test__lease_list(client, lease):
     url = reverse('lease-list')
     response = client.get(url, format='json')
     assert response.status_code == 200
     assert response.data['count']
     response_lease_data = response.data['results'][0]
     assert response_lease_data['id'] == str(lease.id)
+
+
+@mock.patch('umbrella.contracts.views.create_presigned_post', Mock(return_value={}))
+def test__lease_create(client):
+    url = reverse('lease-create')
+    data = {
+        "file_name": "contract.pdf",
+        "file_size": 1024,
+        "file_hash": "contract_hash"
+    }
+    response = client.post(url, data=data, format='json')
+    assert response.status_code == 200
+    assert Lease.objects.count() == 1
 
 
 class LeaseFactory(DjangoModelFactory):
