d4e40b5235cea1025542320eccf03165a9e716ff
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-09 11:41:52 +0300

    Add error handling for parse clause json command

--- umbrella/contracts/management/commands/parse_json.py
+++ umbrella/contracts/management/commands/parse_json.py
@@ -1,15 +1,21 @@
+from pathlib import Path
+
+from django.core.management import CommandError
 from django.core.management.base import BaseCommand
 
 from umbrella.contracts.utils import parse_json
 
 
 class Command(BaseCommand):
-    help = 'Parse json'
+    help = 'Parse clause json'
 
     def add_arguments(self, parser):
-        parser.add_argument('path', type=str)
+        parser.add_argument('file_path', type=str)
 
     def handle(self, *args, **options):
-        path = options['path']
-        parse_json(path)
-        print("Pars successfully")
+        file_path = Path(options['file_path'])
+        if not file_path.is_file():
+            raise CommandError("Invalid file path.")
+
+        parse_json(str(file_path))
+        print(f"File {file_path} parsed successfully")
