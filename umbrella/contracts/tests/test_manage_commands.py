6ead2af03277a8b5087ca2c82293a5bdd2a5c208
Alex-Shvab <apamic1995@gmail.com>
2022-04-14 12:27:11 +0300

    extend manage command test

--- umbrella/contracts/tests/test_manage_commands.py
+++ umbrella/contracts/tests/test_manage_commands.py
@@ -9,6 +9,7 @@ pytestmark = pytest.mark.django_db
 
 TEST_CONTRACT_FILES_DIR = "umbrella/contracts/test_analytics_jsons/6cb7fa02-457f-4e91-be84-3bfea7692d6b"
 TEST_CONTRACT_UUID = "6cb7fa02-457f-4e91-be84-3bfea7692d6b"
+TEST_KDP_TYPE = "start"
 
 
 def test_parse_contract(client):
@@ -16,3 +17,7 @@ def test_parse_contract(client):
     assert Node.objects.count() == 0
     call_command('parse_contract', TEST_CONTRACT_FILES_DIR)
     assert Contract.objects.count() > 0
+    start_kdp = Node.objects.filter(type=TEST_KDP_TYPE).first()
+    assert start_kdp.type == TEST_KDP_TYPE
+    kdp_clause = Node.objects.filter(type=TEST_KDP_TYPE).first()
+    assert kdp_clause is not None
