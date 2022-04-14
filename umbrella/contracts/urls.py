167a419d882c37a271a2fd4aaf32606cd40b1a58
Kuptsov <kuptsovsasha@gmail.com>
2022-04-14 10:37:38 +0300

    rename url

--- umbrella/contracts/urls.py
+++ umbrella/contracts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
          name='aws-contract-processed-webhook'),
     path('<uuid:contract_uuid>/clauses/<str:clause_type>/', KDPClauseView.as_view(), name='kdp_clause'),
 
-    path('get-contracts/', DocumentLibraryListView.as_view(), name='get_contracts'),
+    path('document-library/', DocumentLibraryListView.as_view(), name='get_contracts'),
 
 
 ]
