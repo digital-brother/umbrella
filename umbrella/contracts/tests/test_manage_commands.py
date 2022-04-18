a9c5506ebe6acee608434963972ea82e47287010
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-18 16:27:49 +0300

    Extract TEST_CLAUSE_PARAGRAPH_ID

--- umbrella/contracts/tests/test_manage_commands.py
+++ umbrella/contracts/tests/test_manage_commands.py
@@ -13,6 +13,7 @@ TEST_CONTRACT_FILES_DIR = \
 TEST_CONTRACT_UUID = "6cb7fa02-457f-4e91-be84-3bfea7692d6b"
 TEST_KDP_TYPE = "start"
 TEST_CLAUSE_TYPE = "term"
+TEST_CLAUSE_PARAGRAPH_ID = 24
 
 
 def test_parse_contract_command(client):
@@ -20,9 +21,9 @@ def test_parse_contract_command(client):
     call_command('parse_contract', TEST_CONTRACT_FILES_DIR)
 
     contract_terms = contract.clauses.filter(type=TEST_CLAUSE_TYPE)
-    term = contract_terms.filter(content__paraId=24).first()
+    term = contract_terms.filter(content__paraId=TEST_CLAUSE_PARAGRAPH_ID).first()
     assert term
 
     term_kdps = term.kdps.filter(type=TEST_KDP_TYPE)
-    kdp = term_kdps.filter(content__paraId=24).first()
+    kdp = term_kdps.filter(content__paraId=TEST_CLAUSE_PARAGRAPH_ID).first()
     assert kdp
