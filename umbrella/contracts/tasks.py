1635fe0effd99f2844b13c996bf158aa21da6dff
Alex-Shvab <apamic1995@gmail.com>
2022-04-21 12:14:34 +0300

    celery task use parser from aws

--- umbrella/contracts/tasks.py
+++ umbrella/contracts/tasks.py
@@ -1,11 +1,9 @@
 from celery import shared_task
 
-from umbrella.contracts.utils import download_s3_folder, parse_local_contract
+from umbrella.contracts.utils import parse_aws_contract
 
 
 @shared_task
 def load_aws_analytics_jsons_to_db(contract_uuid):
-    s3_folder = str(contract_uuid).upper()
-    local_folder = download_s3_folder(s3_folder)
-    parse_local_contract(local_folder)
-    return str(local_folder)
+    parse_aws_contract(contract_uuid)
+    return str(contract_uuid)
