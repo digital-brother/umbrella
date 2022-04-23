3603634c50e2feee413b4964f31a2928efc87078
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-24 01:12:16 +0300

    Move asgi.py to config folder

--- config/asgi.py
+++ config/asgi.py
@@ -3,10 +3,11 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
 import umbrella.notifications.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umbrella.config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
