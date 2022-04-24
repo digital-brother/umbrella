71ddd14d8e3449022799c5680ff89965bbbaa5e1
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-24 21:47:26 +0300

    Fix test for NotificationsConsumer

--- umbrella/notifications/tests.py
+++ umbrella/notifications/tests.py
@@ -1,8 +1,10 @@
 import pytest
+from asgiref.sync import sync_to_async
 from channels.testing import WebsocketCommunicator
 
 from umbrella.notifications.consumers import NotificationsConsumer
 from umbrella.notifications.middleware import TokenAuthMiddleware
+from umbrella.notifications.utils import send_message_to_channels_group
 
 pytestmark = [pytest.mark.asyncio, pytest.mark.django_db(transaction=True)]
 
@@ -10,14 +12,15 @@ pytestmark = [pytest.mark.asyncio, pytest.mark.django_db(transaction=True)]
 async def test_notifications_consumer(user):
     url = f'?token={user.auth_token}'
     communicator = WebsocketCommunicator(TokenAuthMiddleware(NotificationsConsumer.as_asgi()), url)
+
     connected, subprotocol = await communicator.connect()
     assert connected
 
     # Test sending text
-    data = {'message': 'hello'}
-    await communicator.send_json_to(data)
+    test_message = 'test_message'
+    await sync_to_async(send_message_to_channels_group)(user.realm, test_message)
     response = await communicator.receive_json_from()
-    assert response['message'] == data['message']
+    assert response['message'] == test_message
 
     # Close
     await communicator.disconnect()
