ae3d05d17a9054afbaf6f4981be2b0403ecccd35
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-24 21:28:04 +0300

    Fix settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -63,7 +63,6 @@ class Common(Configuration):
     ROOT_URLCONF = 'config.urls'
     SECRET_KEY = env("SECRET_KEY", default=None)
     WSGI_APPLICATION = 'config.wsgi.application'
-    WSGI_APPLICATION = 'config.wsgi.application'
     ASGI_APPLICATION = 'config.asgi.application'
     CHANNEL_LAYERS = {
         'default': {
