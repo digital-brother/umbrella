537f050268ff4feb3825476d9f97220163909cd9
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-24 05:25:12 +0300

    Add auth to NotificationsConsumer test

--- umbrella/notifications/tests.py
+++ umbrella/notifications/tests.py
@@ -4,10 +4,12 @@ from channels.testing import WebsocketCommunicator
 from umbrella.notifications.consumers import NotificationsConsumer
 from umbrella.notifications.middleware import TokenAuthMiddleware
 
+pytestmark = [pytest.mark.asyncio, pytest.mark.django_db(transaction=True)]
 
-@pytest.mark.asyncio
-async def test_notifications_consumer():
-    communicator = WebsocketCommunicator(TokenAuthMiddleware(NotificationsConsumer.as_asgi()), '')
+
+async def test_notifications_consumer(user):
+    url = f'?token={user.auth_token}'
+    communicator = WebsocketCommunicator(TokenAuthMiddleware(NotificationsConsumer.as_asgi()), url)
     connected, subprotocol = await communicator.connect()
     assert connected
 
