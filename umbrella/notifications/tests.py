a13b98edd05193aa79deaae834927ffa26e75f0a
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2022-04-24 03:42:56 +0300

    Add auth to test

--- umbrella/notifications/tests.py
+++ umbrella/notifications/tests.py
@@ -2,11 +2,12 @@ import pytest
 from channels.testing import WebsocketCommunicator
 
 from umbrella.notifications.consumers import NotificationsConsumer
+from umbrella.notifications.middleware import TokenAuthMiddleware
 
 
 @pytest.mark.asyncio
 async def test_notifications_consumer():
-    communicator = WebsocketCommunicator(NotificationsConsumer.as_asgi(), "")
+    communicator = WebsocketCommunicator(TokenAuthMiddleware(NotificationsConsumer.as_asgi()), '')
     connected, subprotocol = await communicator.connect()
     assert connected
 
@@ -14,7 +15,7 @@ async def test_notifications_consumer():
     data = {'message': 'hello'}
     await communicator.send_json_to(data)
     response = await communicator.receive_json_from()
-    assert response == data
+    assert response['message'] == data['message']
 
     # Close
     await communicator.disconnect()
