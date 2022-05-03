a6372a318b1f5f2cf48111fae0fbc2fd3b02227d
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-05-03 16:05:27 +0300

    Add basic auth to swagger

--- config/settings/common.py
+++ config/settings/common.py
@@ -243,6 +243,7 @@ class Common(Configuration):
             'umbrella.users.auth.DynamicRealmOIDCAuthentication',
             'rest_framework.authentication.TokenAuthentication',
             'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.BasicAuthentication',
         ),
         'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     }
