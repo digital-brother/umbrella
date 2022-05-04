7ffbcd33d218130b7cebe0463e272321a10ad61f
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-05-05 00:24:45 +0300

    Fix CreateUserSerializer

--- umbrella/users/serializers.py
+++ umbrella/users/serializers.py
@@ -16,7 +16,7 @@ class CreateUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # call create_user on user object. Without this
         # the password will be stored in plain text.
-        user = User.objects.create_user(**validated_data)
+        user = User.create_user_with_default_group(**validated_data)
         return user
 
     class Meta:
