468c13423e09f49321bd62ab9cf20f48cf4c4a7c
Kuptsov <kuptsovsasha@gmail.com>
2022-05-07 10:07:37 +0300

    add debug configurations and add ENV variables with path to sentry

--- config/settings/common.py
+++ config/settings/common.py
@@ -300,10 +300,13 @@ class Common(Configuration):
     # Sentry
     # ------------------------------------------------------------------------------
     sentry_sdk.init(
-        dsn="https://d7f8bb5252194108aff3d7bce773f79e@o1234808.ingest.sentry.io/6384390",
         integrations=[DjangoIntegration()],
-
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production,
         traces_sample_rate=1.0,
         debug=True,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True
     )
