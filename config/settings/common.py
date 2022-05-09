ebe20606d38a63ae5cf6f41d2216d4a391e3cc04
Kuptsov <kuptsovsasha@gmail.com>
2022-05-09 10:10:26 +0300

    remove DNS key.  And set sentry_debug False

--- config/settings/common.py
+++ config/settings/common.py
@@ -305,7 +305,6 @@ class Common(Configuration):
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production,
         traces_sample_rate=1.0,
-        debug=True,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True
