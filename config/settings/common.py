7b87dead0c699f15859501f721be47a0587d8ede
Kuptsov <kuptsovsasha@gmail.com>
2022-05-09 14:56:17 +0300

    add env Variable

--- config/settings/common.py
+++ config/settings/common.py
@@ -307,5 +307,6 @@ class Common(Configuration):
         traces_sample_rate=1.0,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
+        send_default_pii=True,
+        environment="testing",
     )
