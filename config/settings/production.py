729e96fdfec23467984b796cf129db965e619840
Kuptsov <kuptsovsasha@gmail.com>
2022-05-10 12:08:03 +0300

    set enviroment to .local.example

--- config/settings/production.py
+++ config/settings/production.py
@@ -53,5 +53,4 @@ class Production(Common):
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
-        environment="production",
     )
